61cbeb26321bcc8ce7759f44c47bccf242c13b3d
Matheus Cunha <matheuz.a@gmail.com>
2018-12-15 00:53:38 -0200

    feat(*): Moving get_logger to configurations, it should encapsulate and distribute the LOGGER;   Rolling back the decision to use encryption, it isn't working as expected and doesn't deliver value now;   Punches working as expected and being saved to the database

--- app/configurations.py
+++ app/configurations.py
@@ -2,6 +2,22 @@ from configparser import ConfigParser
 from urllib.parse import quote
 from os import path, getenv
 
+import logging
+from pythonjsonlogger import jsonlogger
+
+
+def get_logger():
+    logger = logging.getLogger()
+    configurations = get_configurations()
+
+    logHandler = logging.StreamHandler()
+    formatter = jsonlogger.JsonFormatter()
+    logHandler.setFormatter(formatter)
+    logger.addHandler(logHandler)
+    logger.setLevel(getattr(logging,
+                            configurations.get("logging", "level")))
+    return logger
+
 
 def get_configurations():
     configuration = ConfigParser()

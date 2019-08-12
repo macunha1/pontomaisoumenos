7cad490f4fa5c4a3aba013cbc552bd224449abd8
Matheus Cunha <12192633+macunha1@users.noreply.github.com>
2019-08-12 15:43:43 -0300

    fix(gps): Fixing the GPS Oscilator to avoid wide ranges

--- app/gps.py
+++ app/gps.py
@@ -5,10 +5,10 @@ class GpsOscilator:
     def __init__(self,
                  static_latitude: float,
                  static_longitude: float):
-        self.latitude_arange = numpy.arange(start=static_latitude - 30,
-                                            stop=static_latitude + 30)
-        self.longitude_arange = numpy.arange(start=static_longitude - 30,
-                                             stop=static_longitude + 30)
+        self.latitude_arange = numpy.arange(start=static_latitude - 0.00002,
+                                            stop=static_latitude + 0.00002)
+        self.longitude_arange = numpy.arange(start=static_longitude - 0.00002,
+                                             stop=static_longitude + 0.00002)
 
     def generate_latlong(self) -> (float, float):
         return \

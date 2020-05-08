9f95ca73a62e1fb7cd2370c9717d0341cdb89958
Matheus Cunha <12192633+macunha1@users.noreply.github.com>
2020-05-09 01:41:49 +0200

    refact(*): Removing some unnecessary code
    Deleting a considerable amount of code as the next step towards the
    release of pontomaisoumenos v1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
+from datetime import datetime
+from os import getenv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import exists
+from sqlalchemy import create_engine
+from uuid import uuid4
+
 from .configurations import get_configurations, \
     get_database_connection, get_logger
 from .database.models import User, Punch
@@ -5,36 +12,28 @@ from .database.models import create_tables, drop_tables
 from .gps import GpsOscilator
 from .punches import PunchSimulator
 from .tasks import register_punch
-from datetime import datetime
-
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.sql import exists
-from sqlalchemy import create_engine
-from uuid import uuid4
-from os import getenv
 
 CONFIGURATIONS = get_configurations()
 __DB_ENGINE = create_engine(get_database_connection())
-_db_session = sessionmaker(__DB_ENGINE)()
+database_session = sessionmaker(__DB_ENGINE)()
 logger = get_logger()
 
 
-def get_default_user(user_email: str, default_password: str = "wv&j#7$zd9f#aXUuF"):
+def get_default_user(user_email: str):
     stmt = exists().where(User.email == user_email)
-    if not _db_session.query(User.email).filter(stmt).scalar():
+    if not database_session.query(User.email).filter(stmt).scalar():
 
-        __created_date = datetime.now()
+        created_date = datetime.now()
         default_user = User(id=uuid4(),
                             email=user_email,
-                            password=default_password,
                             active=True,
-                            updated_at=__created_date,
-                            created_at=__created_date)
-        _db_session.add(default_user)
-        _db_session.commit()
-    return (_db_session.query(User)
-                       .filter_by(email=user_email)
-                       .first())
+                            updated_at=created_date,
+                            created_at=created_date)
+        database_session.add(default_user)
+        database_session.commit()
+    return (database_session.query(User)
+            .filter_by(email=user_email)
+            .first())  # Ensures user creation
 
 
 # Sending just one bulk because it will never be greater than 70 values
@@ -42,77 +41,101 @@ def get_default_user(user_email: str, default_password: str = "wv&j#7$zd9f#aXUuF
 # where 0 < month_days <= 31
 def save_punches(from_simulator: PunchSimulator,
                  user: User) -> None:
-    _punches = []
+    punches = []
     for punch_at in from_simulator.generate():
-        _current_time = datetime.now()
-        _punches.append(Punch(
+        current_time = datetime.now()
+        punches.append(Punch(
             id=str(uuid4()),
             user_id=str(user.id),
             should_punch_at=punch_at,
             done=False,
-            updated_at=_current_time,
-            created_at=_current_time))
-    _db_session.add_all(_punches)
-    _db_session.commit()
+            updated_at=current_time,
+            created_at=current_time))
+    database_session.add_all(punches)
+    database_session.commit()
+    return punches
 
 
-def main() -> None:
-    __CURRENT_DATE = datetime.now()
+def generate_monthly_working_hours() -> None:
+    CURRENT_DATE = datetime.now()
     # Possible timerange to control the "work journey"
-    punch_it = PunchSimulator(
-        possible_minutes_start=CONFIGURATIONS.getint(
-            "working_hours",
-            "possible_minutes_variation_start"),
-        possible_minutes_stop=CONFIGURATIONS.getint(
-            "working_hours",
-            "possible_minutes_variation_end"),
-        possible_start_hours=CONFIGURATIONS.getint("working_hours",
-                                                   "possible_start_hour"),
-        possible_start_hours_variation=CONFIGURATIONS.getfloat(
-            "working_hours",
-            "accepted_start_hour_variation"),
-        expected_daily_hours=CONFIGURATIONS.getfloat("working_hours",
-                                                     "expected_daily_hours"),
-        max_daily_hours=CONFIGURATIONS.getint("working_hours",
-                                              "maximum_daily_working_hours"),
-        lunch_time=CONFIGURATIONS.getint("working_hours", "lunch_time"),
-        target_month=int(getenv("TARGET_MONTH") or __CURRENT_DATE.month),
-        target_year=int(getenv("TARGET_YEAR") or __CURRENT_DATE.year)
-    )
+    punch_it = PunchSimulator(possible_minutes_start=CONFIGURATIONS
+                              .getint("working_hours",
+                                      "possible_minutes_variation_start"),
+                              possible_minutes_stop=CONFIGURATIONS
+                              .getint("working_hours",
+                                      "possible_minutes_variation_end"),
+                              possible_start_hours=CONFIGURATIONS
+                              .getint("working_hours",
+                                      "possible_start_hour"),
+                              possible_start_hours_variation=CONFIGURATIONS
+                              .getfloat(
+                                  "working_hours",
+                                  "accepted_start_hour_variation"),
+                              expected_daily_hours=CONFIGURATIONS
+                              .getfloat("working_hours",
+                                        "expected_daily_hours"),
+                              max_daily_hours=CONFIGURATIONS
+                              .getint("working_hours",
+                                      "maximum_daily_working_hours"),
+                              lunch_time=CONFIGURATIONS
+                              .getint("working_hours",
+                                      "lunch_time"),
+                              target_month=int(getenv("TARGET_MONTH") or
+                                               CURRENT_DATE.month),
+                              target_year=int(getenv("TARGET_YEAR") or
+                                              CURRENT_DATE.year))
+
     # Reads the current execution user from database
     current_user = get_default_user(CONFIGURATIONS.get("user", "email"),
                                     CONFIGURATIONS.get("user", "password"))
 
     # Generates a random geolocation based on the latitude and longitude configured
-    gps = GpsOscilator(static_latitude=CONFIGURATIONS.getfloat("location", "latitude"),
-                       static_longitude=CONFIGURATIONS.getfloat("location", "longitude"))
+    gps = GpsOscilator(static_latitude=CONFIGURATIONS.getfloat("location",
+                                                               "latitude"),
+                       static_longitude=CONFIGURATIONS.getfloat("location",
+                                                                "longitude"))
     static_latitude, static_longitude = gps.generate_latlong()
     user_work_address = CONFIGURATIONS.get("location", "address")
 
     # Save punches for this user on database
-    save_punches(from_simulator=punch_it,
-                 user=current_user)
-    __values = _db_session.query(Punch) \
-                          .filter_by(user_id=str(current_user.id)) \
-                          .all()
+    return save_punches(from_simulator=punch_it,
+                        user=current_user)
 
-    # Schedule the pendent punches with dramatiq
-    for _result in __values:
-        if _result.should_punch_at < __CURRENT_DATE:
+
+def schedule_punches() -> None:
+    CURRENT_DATE = datetime.now()
+
+    scheduled_punches = database_session \
+        .query(Punch) \
+        .filter_by(user_id=str(current_user.id)) \
+        .all() or generate_monthly_working_hours()
+
+    # Schedule only the pending punches (the ones with future dates)
+    # Change the implementation from Dramatiq to APScheduler
+    # Ref: https://apscheduler.readthedocs.io/en/3.0/
+    for scheduled_punch in scheduled_punches:
+        if scheduled_punch.should_punch_at < CURRENT_DATE:
             continue
-        _key = "schedule-punch-for-{user}-at-{date}".format(
-            user=current_user.id,
-            date=_result.should_punch_at.strftime("%d-%m-%y-%H:%M"))
-        punch_at = _result.should_punch_at - datetime.now()
-        logger.info("Registering value for punch %s in %s" %
-                    (_key, round(punch_at.total_seconds() * 1000)))
-        register_punch.send_with_options(
-            args=(current_user.email,
-                  current_user.password,
-                  user_work_address,
-                  static_latitude,
-                  static_longitude),
-            delay=round(punch_at.total_seconds() * 1000))
+
+        punch_at = scheduled_punch.should_punch_at - datetime.now()
+        logger.info("%s scheduled to punch at %s" %
+                    (current_user.id,
+                     scheduled_punch.should_punch_at.isoformat()))
+
+        register_punch.send_with_options(args=(current_user.email,
+                                               current_user.password,
+                                               user_work_address,
+                                               static_latitude,
+                                               static_longitude),
+                                         delay=round(punch_at
+                                                     .total_seconds() *
+                                                     1000))
+
+
+def main() -> None:
+    generate_monthly_working_hours()
+    schedule_punches()
 
 
 if __name__ == "__main__":
